f8318ba9d6ac3debd786d8bfe4ee3bb0838addf0
Tiago Santini <66257666+tiagosantini@users.noreply.github.com>
2026-09-17 20:39:39 -0300

    Adiciona Migration para TBInstrutores

---
+++ WebApp/Modulos/ModuloInstrutor/Infraestrutura/InstrutorConfiguration.cs
@@ -0,0 +1,35 @@
+using EscolaDeCursos.WebApp.Modulos.ModuloInstrutor.Dominio;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EscolaDeCursos.WebApp.Modulos.ModuloInstrutor.Infraestrutura;
+
+public sealed class InstrutorConfiguration : IEntityTypeConfiguration<Instrutor>
+{
+    public void Configure(EntityTypeBuilder<Instrutor> builder)
+    {
+        builder.ToTable("TBInstrutores");
+
+        // Colunas da Tabela TBInstrutores
+        builder.HasKey(i => i.Id); // Chave Primária
+        builder.Property(i => i.Id).ValueGeneratedNever();
+
+        builder.Property(i => i.Nome)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        builder.Property(i => i.Telefone)
+            .HasMaxLength(20)
+            .IsRequired();
+
+        builder.Property(i => i.Cpf)
+            .HasMaxLength(14)
+            .IsRequired();
+
+        builder.HasIndex(i => i.Telefone)
+            .IsUnique();
+
+        builder.HasIndex(i => i.Cpf)
+            .IsUnique();
+    }
+}
